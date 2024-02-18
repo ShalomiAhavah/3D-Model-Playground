68b7cdf242818d95a8a9ee63d2b6c35948512e81
Julia Gaugler <greengates4j@gmail.com>
2024-02-18 21:49:47 +0200

    Installed three.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-import * as THREE from "three";
-
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.135.0/build/three.module.js";
 var model = document.querySelector(".viewer");
 var modelColor = document.querySelector(".viewer#color");
 
 var colorButtons = document.querySelector(".color-changes");
 
 const loader = new THREE.GLTFLoader();
+console.log(typeof loader);
