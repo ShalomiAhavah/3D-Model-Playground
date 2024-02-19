cfc76dc40308d30d865ac4e59f28c2434ef1e693
Julia Gaugler <greengates4j@gmail.com>
2024-02-19 21:57:14 +0200

    Metallic & Color change

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.135.0/build/three.module.js";
-var model = document.querySelector(".viewer");
-var modelColor = document.querySelector(".viewer#color");
+const modelViewerColor = document.querySelector("model-viewer#color");
 
-var colorButtons = document.querySelector(".color-changes");
-
-const loader = new THREE.GLTFLoader();
-console.log(typeof loader);
+document.querySelector(".color-changes").addEventListener("click", (event) => {
+  const colorString = event.target.dataset.color;
+  const [material] = modelViewerColor.model.materials;
+  material.pbrMetallicRoughness.setBaseColorFactor(colorString);
+});
