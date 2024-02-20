667b09bead3359c0dbf7952236a168bfc2f40d24
Julia Gaugler <greengates4j@gmail.com>
2024-02-20 18:59:29 +0200

    Unneccessary Delete

--- script.js
+++
@@ -1,7 +0,0 @@
-const modelViewerColor = document.querySelector("model-viewer#color");
-
-document.querySelector(".color-changes").addEventListener("click", (event) => {
-  const colorString = event.target.dataset.color;
-  const [material] = modelViewerColor.model.materials;
-  material.pbrMetallicRoughness.setBaseColorFactor(colorString);
-});
